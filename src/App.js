c0e807be888f45b8177f9a36ce50bd3ccc2f2ed0
Valentin <Valentin@orange.fr>
2020-02-24 16:16:05 +0100

    ROMAIN PAS DE ZIP

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-//import Map from "./map";
-//import { Marker } from 'leaflet';
-import { render } from 'react-dom'
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import MapView from './map.js';
 
-const position =[49.1191, 6.1727]
 export default class App extends React.Component{
 
   constructor(props){
     super(props);
 
     this.state = {
-
     }
+
   }
 
   render(){
     return(
-      <div>
-      <Map center={position} zoom={13}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-        />
-        <Marker position={position}>
-          <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-        </Marker>
-      </Map>
-      </div>
+      <MapView></MapView>
     )
   }
 }
---
+++ src/map.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+
+const position = [37.335556, -122.009167];
+
+export default class MapView extends React.Component {
+    render() {
+        return (
+                  <div
+                    style={{
+                        height:"100%"
+                    }}>
+                    <Map center={position} zoom={13}>
+                        <TileLayer
+                          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        />
+                        <Marker position={position}>
+                          <Popup>
+                            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+                          </Popup>
+                        </Marker>
+                      </Map>
+                  </div>
+        );
+    }
+}
