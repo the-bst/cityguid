1931436136531e07de67c45032628960d5dcb4ac
Ornael <vincent.arioli@gmail.com>
2020-02-25 15:54:50 +0100

    Merge + Geolocalisation des bat + Geolocalisation user

--- src/map.js
+++ src/map.js
@@ -4,6 +4,7 @@ import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import "./Design/map.css";
 import L from "leaflet";
 import icon from "./BEBER.png";
+import axios from 'axios';
 
 var myIcon = L.icon({
   iconUrl: icon,
@@ -19,20 +20,33 @@ const center_map = [49.1191, 6.1727];
 
 export default class MapView extends Component {
 
-  constructor(props){
+  
+  constructor(props) {
     super(props);
     const watcher = navigator.geolocation.watchPosition(this.displayLocationInfo);
     this.state = {
-      position:[0,0],
+      position: [0, 0],
+      Place: [],
     }
   }
+  componentDidMount() {
 
-start=()=>{
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
+    axios.get("https://devweb.iutmetz.univ-lorraine.fr/~schnei349u/api_react/listelieu.php")
+      .then(response => {
+        this.setState({ Place: response.data })
+        console.log(response.data);
+        //console.log(this.state.Place[0].coord_nord)
+
+      })
   }
-}
-
+  start = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.displayLocationInfo);
+    }
+  }
+  setTimeout = (() => {
+    navigator.geolocation.clearWatch(this.watcher);
+  }, 15000);
  displayLocationInfo = (position) =>{
   const lng = position.coords.longitude;
   const lat = position.coords.latitude;
@@ -48,8 +62,6 @@ setTimeout = (() => {
   navigator.geolocation.clearWatch(this.watcher);
 }, 15000);
 
-
-
   render() {
     return (
       <Map center={center_map} zoom={13} className="Map">
@@ -63,6 +75,13 @@ setTimeout = (() => {
             <span>C EST BEBER</span>
           </Popup>
         </Marker>
+        {
+          this.state.Place.map(
+            (Lieu, index) =>
+              <Marker position={[Lieu.coord_nord, Lieu.coord_est]} icon={myIcon} className="Marker"> </Marker>
+
+          )
+        }
       </Map>
     );
   }
