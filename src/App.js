97e3b32f8f921bc362980e4bde8634da44f18bd1
RFrancois6 <Romain57890@orange.fr>
2020-02-25 14:28:22 +0100

    tg valentin

--- src/App.js
+++ src/App.js
@@ -13,17 +13,8 @@ export default class App extends React.Component{
 
   }
 
-  render(){
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(displayLocationInfo);
-    }
-    
-    function displayLocationInfo(position) {
-      const lng = position.coords.longitude;
-      const lat = position.coords.latitude;
-    
-      console.log(`longitude: ${ lng } | latitude: ${ lat }`);
-    }
+  render(){    
+   
     return(
       <div>
         <Sider className = "Sider"></Sider>
--- src/map.js
+++ src/map.js
@@ -1,36 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-import './Design/map.css';
-import L from 'leaflet';
-import icon from './BEBER.png'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import "./Design/map.css";
+import L from "leaflet";
+import icon from "./BEBER.png";
 
 var myIcon = L.icon({
   iconUrl: icon,
   iconSize: [38, 95],
   iconAnchor: [22, 94],
   popupAnchor: [-3, -76],
-  shadowUrl: 'my-icon-shadow.png',
+  shadowUrl: "my-icon-shadow.png",
   shadowSize: [68, 95],
   shadowAnchor: [22, 94]
 });
 
-const position = [49.1191, 6.1727];
+function displayLocationInfo(position) {
+  const lng = position.coords.longitude;
+  const lat = position.coords.latitude;
+
+  //console.log(`longitude: ${ lng } | latitude: ${ lat }`);
+}
+
+const center_map = [49.1191, 6.1727];
+
+setTimeout(() => {
+  navigator.geolocation.clearWatch(center_map);
+}, 15000);
 
 export default class MapView extends React.Component {
-    render() {
-        return (
-                    <Map center={position} zoom={13} className="Map">
-                        <TileLayer
-                          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        />
-                        <Marker position={position} icon={myIcon} className="Marker">
-                          <Popup>
-                            <span>C EST BEBER</span>
-                          </Popup>
-                        </Marker>
-                      </Map>
-        );
-    }
+  render() {
+    return (
+      <Map center={center_map} zoom={13} className="Map">
+        <TileLayer
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={center_map} icon={myIcon} className="Marker">
+          <Popup>
+            <span>C EST BEBER</span>
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
 }
