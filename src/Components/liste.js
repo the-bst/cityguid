e0d75f816ad4f8306ecbf23d8c0548fbdf51cc3e
RFrancois6 <Romain57890@orange.fr>
2020-02-26 15:43:58 +0100

    joli menu

--- src/Components/liste.js
+++ src/Components/liste.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../Design/map.css";
 import axios from 'axios';
+import "../Design/liste.css";
 
 export default class List extends React.Component {
     constructor(props) {
@@ -24,7 +25,13 @@ export default class List extends React.Component {
             {
                 this.state.Place.map(
                     (Lieu, index) =>
-                        <p>{Lieu.nom_lieux} <a href={Lieu.lien}>Lien wikipédia</a></p>
+                    <section className="liste_monuments">
+                        <h1>{Lieu.nom_lieux}</h1>
+                        <img src={Lieu.image} className="bdd_img"/>
+                        <p className="bdd_desc">{Lieu.description}</p>
+                        <br />
+                        <a href={Lieu.lien}>Pour en savoir plus...</a>
+                    </section>
                 )
             }
             </div>
