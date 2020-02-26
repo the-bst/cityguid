996d723a48977b442d06bfe97e44748eceaa9836
basti <bastien.collignon@outlook.fr>
2020-02-26 11:35:20 +0100

    nouveau menu , pas fini

--- src/Components/menu.js
+++ src/Components/menu.js
@@ -6,16 +6,35 @@ import axios from 'axios';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';
+import { Button, Icon } from 'antd';
+
+//const { Button, Icon } = antd;
+
+const ButtonGroup = Button.Group;
 
 export default class Sider extends React.Component {
-  render() {
-    return (
-      <DropdownButton id="dropdown-basic-button" title="Menu">
-        <Dropdown.Item href="#" onClick={() => {this.props.showListeView()}}>Liste des monuments</Dropdown.Item>
-        <Dropdown.Item href="#" onClick={() => {this.props.showMapView()}}>Carte</Dropdown.Item>
-      </DropdownButton> 
-    )
-  }
-}
+    render() {
+        return (
+            /*
+          <DropdownButton id="dropdown-basic-button" title="Menu">
+            <Dropdown.Item href="#" onClick={() => {this.props.showListeView()}}>Liste des monuments</Dropdown.Item>
+            <Dropdown.Item href="#" onClick={() => {this.props.showMapView()}}>Carte</Dropdown.Item>
+          </DropdownButton> 
+        )
+        }*/
 
-//ReactDOM.render(<Sider />, document.getElementById('root'));
+
+            <ButtonGroup>
+                <Button type="primary" onClick={() => { this.props.showMapView() }}>
+                    <Icon type="left" />
+                    Map
+      </Button>
+                <Button type="primary" onClick={() => { this.props.showListeView() }}>
+                    Liste Monuments
+        <Icon type="right" />
+                </Button>
+            </ButtonGroup>
+        )
+    }
+}
