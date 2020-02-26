2d226b93a290641277e2d6170d89c16f06861c87
Ornael <vincent.arioli@gmail.com>
2020-02-26 16:24:41 +0100

    Prise en charge multilieux proche

--- src/Components/map.js
+++ src/Components/map.js
@@ -6,7 +6,6 @@ import L from "leaflet";
 import icon from "../location-arrow-solid.svg";
 import axios from 'axios';
 import marq from '../marqueur.png';
-import distanceTo from "leaflet";
 
 var myicon = L.icon({
   iconUrl: icon,
@@ -32,11 +31,34 @@ export default class MapView extends Component {
       liste_bat: {},
     }
   }
+  batiment_proche = (liste_bati_dist) => {
+    // console.log(liste_bati_dist);
+    var nearest = 101;
+    var nom;
+    // console.log(Object.keys(liste_bati_dist).length);
+    if(Object.keys(liste_bati_dist).length == 1){
+      for(var key in liste_bati_dist){
+        alert("Vous êtes proche de "+ key);
+      }
+    }
+    else if(Object.keys(liste_bati_dist).length > 1){
+    for(var key in liste_bati_dist){
+      if(liste_bati_dist[key] < nearest){
+        nearest = liste_bati_dist[key];
+        // console.log(nearest);
+        nom = key;
+        // console.log(nom);
+      }
+    }
+    alert("Vous êtes proche de "+ nom);
+  }
+        
+}
   get_monument = async () => {
     await axios.get("https://devweb.iutmetz.univ-lorraine.fr/~schnei349u/api_react/listelieu.php")
       .then(response => {
         this.setState({ Place: response.data })
-        console.log(response.data);
+        //console.log(response.data);
         let liste_bat = {};
         this.state.Place.map(
           (Lieu, index) => {
@@ -44,7 +66,6 @@ export default class MapView extends Component {
           }
         )
         this.setState({ liste_bat });
-        console.log(this.state.liste_bat);
       });
 
   }
@@ -64,6 +85,7 @@ export default class MapView extends Component {
     const lng = position.coords.longitude;
     const lat = position.coords.latitude;
     var position = [];
+    var tab_bat_dist = {};
     position.push(lat);
     position.push(lng);
     this.setState({ position, });
@@ -71,19 +93,21 @@ export default class MapView extends Component {
     this.state.Place.map(
       (Lieu, index) => {
         var latlng2 = L.latLng(Lieu.coord_nord, Lieu.coord_est);
-        if (latlng2.distanceTo(latlng) < 3000) {
+        var distance = latlng2.distanceTo(latlng);
+        if (distance < 100) {
           if (this.state.liste_bat[Lieu.nom_lieux] == 0) {
-            alert("Test");
+            tab_bat_dist[Lieu.nom_lieux] = distance;
             this.state.liste_bat[Lieu.nom_lieux] = 1;
           }
-          else {
-            if (this.state.liste_bat[Lieu.nom_lieux] == 1) {
-              this.state.liste_bat[Lieu.nom_lieux] = 0;
-            }
+        }
+        else {
+          if (this.state.liste_bat[Lieu.nom_lieux] == 1) {
+            this.state.liste_bat[Lieu.nom_lieux] = 0;
           }
         }
       }
     )
+    this.batiment_proche(tab_bat_dist);
   }
 
   setTimeout = (() => {
@@ -105,7 +129,7 @@ export default class MapView extends Component {
         {
           this.state.Place.map(
             (Lieu, index) =>
-              <Marker position={[Lieu.coord_nord, Lieu.coord_est]} icon={mymarq} className="Marker">
+              <Marker position={[Lieu.coord_nord, Lieu.coord_est]} icon={mymarq} key={Lieu.nom_lieux} className="Marker">
                 <Popup>
                   <span>{Lieu.nom_lieux}</span>
                 </Popup>
