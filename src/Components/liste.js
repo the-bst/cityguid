623cbaa65dffd9b644e337aadd08bd02adc04461
RFrancois6 <Romain57890@orange.fr>
2020-02-27 16:57:49 +0100

    encore du css

--- src/Components/liste.js
+++ src/Components/liste.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import "../Design/liste.css";
 import 'antd/dist/antd.css';
 import { Carousel } from 'antd';
+import Fade from 'react-reveal/Fade';
 
 export default class List extends React.Component {
     constructor(props) {
@@ -23,25 +24,33 @@ export default class List extends React.Component {
 
     render() {
         return (
+            
             <div className="all_list">
             {
                 this.state.Place.map(
                     (Lieu, index) =>
                     <section className="monument_part">
-                        <h1 className="title_mon">{Lieu.nom_lieux}</h1>
+                        <Fade left>
+                            <h1 className="title_mon">{Lieu.nom_lieux}</h1>
+                        </Fade>
+                        <Fade right>
                         <Carousel autoplay>
                             <img src={Lieu.image} className="img_mon"/>
                             <img src={Lieu.image2} className="img_mon"/>
                             <img src={Lieu.image3} className="img_mon"/>
                         </Carousel>
+                        </Fade>
+                        <Fade>
                         <div className="description_bg">
                           <p className="description_text">{Lieu.description}</p>
                           <a href={Lieu.lien} className="wiki_link">Pour en savoir plus...</a>
                         </div>
+                        </Fade>
                     </section>
                 )
             }
             </div>
+            
         )
     }
 }
