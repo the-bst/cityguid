0184fadac50dd24fa8a341bde4c5ad435eecc515
Valentin <Valentin@orange.fr>
2020-02-27 19:05:53 +0100

    reparation

--- src/Components/liste.js
+++ src/Components/liste.js
@@ -6,96 +6,6 @@ import "antd/dist/antd.css";
 import { Carousel, Input } from "antd";
 import Fade from "react-reveal";
 const { Search } = Input;
-<<<<<<< HEAD
-export default class List extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            Place: [],
-            Textee : "",
-            error : false,
-            value :"",
-        }
-    }
-
-    getData = async () =>{
-      await axios.get("https://devweb.iutmetz.univ-lorraine.fr/~schnei349u/api_react/listelieu.php")
-          .then(response => {
-              this.setState({ Place: response.data })
-
-          })
-    }
-    componentDidMount() {
-      this.getData();
-    }
-    handleChange = async (event) => {
-        this.errorOn();
-        var Textee =await event.target.value.toLowerCase();
-        this.setState({ Textee })
-        //console.log("Erreur avant map :"+ this.state.error);
-        this.state.Place.map(
-            (Lieu, index) =>
-            {
-              //console.log("taille textee "+this.state.Textee.lengt);
-               if (!this.state.Textee==""){
-                if (Lieu.nom_lieux.toLowerCase().includes(this.state.Textee.toLowerCase())) {
-                  this.errorOff();
-      }
-    }
-    }
-    );
-    //console.log("Erreur après map :"+ this.state.error);
-    if (!this.state.Textee==""){
-      this.setState({value:Textee});
-      this.checkError();
-    }
-  }
-
-    errorOn = () =>{
-      if(!this.state.error){
-        this.setState({error:true});
-      }
-    }
-
-    errorOff = () =>{
-      if(this.state.error){
-        this.setState({error:false});
-      }
-    }
-
-    checkError = () =>{
-      if(this.state.error){
-        alert("Aucun batiment avec ce nom");
-          this.setState({value:"",Textee:""});
-      }
-    }
-
-
-    render() {
-        return (
-            <div>
-            <div>
-            <Search
-              onChange={this.handleChange}
-              placeholder="Rechercher..."
-              size="large"
-              value={this.state.value}
-            />
-            </div>
-            <div className="all_list">
-            {
-
-                this.state.Place.map(
-                    (Lieu, index) =>
-                    { if (this.state.Textee==="")
-
-                        return <section className="monument_part">
-
-                        <h1 className="title_mon">{Lieu.nom_lieux}</h1>
-                        <Carousel autoplay>
-=======
->>>>>>> 7a5d2ad555207fda55679c545da93b56eee0a0ae
 
 export default class List extends React.Component {
   constructor(props) {
@@ -184,14 +94,6 @@ export default class List extends React.Component {
                 </section>
               );
             }
-<<<<<<< HEAD
-
-
-            </div>
-            </div>
-    )
-    }
-=======
             //else if (this.state.Erreur == false)
             //{
             //    alert("Erreur")
@@ -201,5 +103,4 @@ export default class List extends React.Component {
       </div>
     );
   }
->>>>>>> 7a5d2ad555207fda55679c545da93b56eee0a0ae
 }
